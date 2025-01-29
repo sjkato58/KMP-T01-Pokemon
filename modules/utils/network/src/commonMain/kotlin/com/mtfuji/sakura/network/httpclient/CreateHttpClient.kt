f4fd02d975c49fe0f888f2d63f152899c0087733
sjkato831 <sjkato83@gmail.com>
2025-01-29 14:13:56 +0000

    Update: minor code update to check how token works with ktor

--- modules/utils/network/src/commonMain/kotlin/com/mtfuji/sakura/network/httpclient/CreateHttpClient.kt
+++ modules/utils/network/src/commonMain/kotlin/com/mtfuji/sakura/network/httpclient/CreateHttpClient.kt
@@ -2,9 +2,14 @@ package com.mtfuji.sakura.network.httpclient
 
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BearerTokens
+import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.header
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 
@@ -19,6 +24,14 @@ fun createHttpClient(engine: HttpClientEngine): HttpClient {
                     ignoreUnknownKeys = true//Prevents app from crashing if detects json fields not accounted for
                 }
             )
+        }
+        install(Auth) {
+            bearer {
+                BearerTokens("", "")
+            }
+        }
+        defaultRequest {
+            //header("Authorization", "Bearer your-api-key")
         }
     }
 }
